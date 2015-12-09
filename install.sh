efbe0ef3f63986ba2abdbb65af87d35a0e79ec70
rufpierre <rufpierre1@gmail.com>
2015-12-09 09:26:48 +0000

    dot prefix

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo apt-get install -y git
 sudo apt-get install -y vim
 sudo apt-get install -y exuberant-ctags
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+sudo apt-get install -y tmux
