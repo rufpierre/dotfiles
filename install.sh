352486848eb1f0dd443fe580857e68a0e7729e91
rufpierre <rufpierre1@gmail.com>
2015-12-09 09:28:47 +0000

    git config

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ sudo apt-get install -y vim
 sudo apt-get install -y exuberant-ctags
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get install -y tmux
+
+git config --global user.name rufpierre
+git config --global user.email rufpierre1@gmail.com
+git config --global color.ui auto
+git config -l
